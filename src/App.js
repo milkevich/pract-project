7d67aed338213b2930185889f48e28fcf4bad783
Gleb Milkevich <glebmilkevich@icloud.com>
2023-10-11 16:53:07 -0400

    mui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import { TextField, Button } from '@mui/material';
+import { TextField } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <Button variant="contained">Log In</Button>
     </div>
   );
 }
