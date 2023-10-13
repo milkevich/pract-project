7040af809b1f6dc7083e177efe28bf1d4b4ca2ce
Gleb Milkevich <glebmilkevich@icloud.com>
2023-10-12 21:09:55 -0400

    LogIn Page finished

--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -2,16 +2,90 @@ import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
+import { useState, useEffect } from 'react';
 
 function LogIn() {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  const [formReady, setFormReady] = useState(false);
+
+  useEffect(() => {
+    setFormReady(!emailError && !passwordError && (email !== '' && password !== ''));
+  }, [email, password, emailError, passwordError]);
+
+  const emailValidation = (e) => {
+    const pattern = /^[^]+@[^]+\.[a-z]{2,3}$/;
+    const emailValue = e.target.value;
+    setEmail(emailValue);
+
+    if (emailValue === '' || !emailValue.match(pattern)) {
+      setEmailError(true);
+      setEmailErrorMessage('Please enter a valid email address');
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage('');
+    }
+  };
+
+  const passwordValidation = (e) => {
+    const passwordValue = e.target.value;
+    setPassword(passwordValue);
+
+    if (passwordValue === '' || passwordValue.length < 6) {
+      setPasswordError(true);
+      setPasswordErrorMessage('You must have at least 6 characters');
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage('');
+    }
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (!emailError || !passwordError) {
+      alert('велкам ❤️');
+    }
+  };
+
   return (
     <div>
-            <CssBaseline />
-                <Container maxWidth="sm">
-                    <TextField required sx={{position: 'relative', marginTop: '100px', width: '300px'}} id="outlined-basic" label="Email" variant="outlined"></TextField>
-                    <TextField required sx={{position: 'relative', marginTop: '20px', width: '300px'}} id="outlined-basic" label="Password" variant="outlined"></TextField>
-                    <Button sx={{position: 'relative', marginTop: '20px', width: '300px'}} variant="contained">Log In</Button>
-                </Container>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <TextField
+          error={emailError}
+          helperText={emailErrorMessage}
+          id="email"
+          onChange={emailValidation}
+          value={email}
+          required
+          sx={{ position: 'relative', marginTop: '100px', width: '300px' }}
+          label="Email"
+          variant="outlined"
+        />
+        <TextField
+          error={passwordError}
+          helperText={passwordErrorMessage}
+          type="password"
+          required
+          sx={{ position: 'relative', marginTop: '20px', width: '300px' }}
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          onChange={passwordValidation}
+        />
+        <Button
+          onClick={submit}
+          sx={{ position: 'relative', marginTop: '20px', width: '300px' }}
+          variant="contained"
+          disabled={!formReady}
+        >
+          Log In
+        </Button>
+      </Container>
     </div>
   );
 }
